2b4bafeaac9ee348f4bceae8a8847f7d6afdf6b5
kimjy8937 <kimjy3520@gmail.com>
2025-04-17 14:38:05 +0900

    feat: user-service FeignClient 경로 수정

--- newbit-user-service/src/main/java/com/newbit/newbituserservice/common/config/FeignClientConfig.java
+++ newbit-user-service/src/main/java/com/newbit/newbituserservice/common/config/FeignClientConfig.java
@@ -34,9 +34,11 @@ public class FeignClientConfig {
 
                 // 2. 내부 user service를 요청하는 상황
                 String userId = requestAttributes.getRequest().getHeader("X-User-Id");
-                String role = requestAttributes.getRequest().getHeader("X-User-Role");
+                String authority = requestAttributes.getRequest().getHeader("X-User-Authority");
+                String username = requestAttributes.getRequest().getHeader("X-User-Email");
                 requestTemplate.header("X-User-Id", userId);
-                requestTemplate.header("X-User-Role", role);
+                requestTemplate.header("X-User-Authority", authority);
+                requestTemplate.header("X-User-Email", username);
             }
         };
     }
