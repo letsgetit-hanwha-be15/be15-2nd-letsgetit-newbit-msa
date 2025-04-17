2c1e964371519fd70de936ff6ae96c0d9bcc6dc6
sezeme <stellaa223@gmail.com>
2025-04-17 11:48:38 +0900

    feat: config 서버 추가

---
+++ newbit-config-server/src/main/java/com/ohgiraffers/newbitconfigserver/NewbitConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.newbitconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbitConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbitConfigServerApplication.class, args);
+    }
+
+}
